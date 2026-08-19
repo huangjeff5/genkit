aad525f899cefa1ceb36590ac36a269a0aac2a71
Jeff Huang <huangjeff@google.com>
2026-08-18 20:54:45 -0500

    remove ConfigArg

--- py/packages/genkit/src/genkit/_ai/_model.py
+++ py/packages/genkit/src/genkit/_ai/_model.py
@@ -53,7 +53,6 @@ ModelFn = Callable[[ModelRequest, ActionRunContext], Awaitable[ModelResponse[Any
 
 # Veneer-facing argument shapes. Internals resolve these into ResolvedModel.
 ModelArg: TypeAlias = str | ModelRef[BaseModel]
-ConfigArg: TypeAlias = BaseModel | Mapping[str, Any]
 
 
 @dataclass(frozen=True, kw_only=True)
